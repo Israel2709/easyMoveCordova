14d1fba07d87b569cc957d689300e5932464fb62
unknown <israel.salinas.m@gmail.com>
2016-08-03 11:07:20 -0500

    last commit

---
+++ www/app/scripts/controllers/manager.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name easyMoveApp.controller:ManagerCtrl
+ * @description
+ * # ManagerCtrl
+ * Controller of the easyMoveApp
+ */
+angular.module('easyMoveApp')
+  .controller('ManagerCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    alert("manager")
+  });
